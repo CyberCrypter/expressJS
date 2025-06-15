d1c2c962a729a2c2aec2bc524043b017b32c8536
Jiten <jitendas9574@gmail.com>
2025-06-15 14:12:40 +0530

    separate routes

--- controller.js
+++ controller.js
@@ -1,9 +1,6 @@
-export const usernameController = (req,res)=> {
-    const username = req.params.name;
-    res.send(`Hello ${username}`);
+export const userLogin = (req,res) =>{
+    res.send("This is user login page");
 }
-
-export const searchController = (req,res)=>{
-    const word = req.query.keyword;
-    res.send(`searching for ${word}`)
+export const userSignup = (req,res) =>{
+    res.send("This is user signup page");
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import  express from 'express';
-import { searchController, usernameController } from './controller.js';
-
+import router from './route.js';
 const app = express()
 
 const PORT = 3000
@@ -9,12 +8,8 @@ app.get('/', (req,res) => {
     res.send("Hello word")
 })
 
-//dynamic route
-app.get('/user/:name', usernameController)
-
-//search?keyword=express
-app.get('/search', searchController)
+app.use('/user',router)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+}) 
